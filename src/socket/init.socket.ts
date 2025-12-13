7a34649d7337485acf551f54951cecbfea300f1b
THARUNJP <tharunroshini71@gmail.com>
2025-12-13 19:03:09 +0530

    issue fix

--- src/socket/init.socket.ts
+++ src/socket/init.socket.ts
@@ -3,13 +3,14 @@ import { Server as HttpServer } from "http";
 
 let io: Server;
 
-export function initSocket(server: HttpServer): Server {
+export default function initSocket(server: HttpServer): Server {
   io = new Server(server, {
     cors: {
       origin: "http://localhost:3000",
       methods: ["GET", "POST"],
     },
   });
+console.log("socket is running on port 8000");
 
   io.on("connection", (socket) => {
     console.log("Socket connected:", socket.id);
